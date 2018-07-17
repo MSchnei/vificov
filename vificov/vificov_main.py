First working implementation. - Two methods, add and max, are implemented and working. - Correct normalization of color maps. - Bootstrapping across voxels.

vificov/vificov_main.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 """Main function for vificov, which is called from command line."""
 
@@ -24,14 +23,14 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 from vificov.load_config import load_config
-from vificov.vificov_utils import (cls_set_config, loadNiiDataExt,
-                                   rmp_deg_pixel_x_y_s, crt_2D_gauss)
+from vificov.vificov_utils import (cls_set_config, loadNiiDataExt, crt_fov,
+                                   rmp_deg_pixel_xys, bootstrap_resample)
 
 
 def run_vificov(strCsvCnfg):
     ###########################################################################
     ## debugging
-    #strCsvCnfg = '/home/marian/Documents/Git/vificov/config_custom.csv'
+    #strCsvCnfg = '/home/marian/Documents/Git/vificov/vificov/config_custom.csv'
     ###########################################################################
     # %% Load parameters and files
 
@@ -41,76 +40,153 @@ def run_vificov(strCsvCnfg):
     # Load config parameters from dictionary into namespace:
     cfg = cls_set_config(dicCnfg)
 
-    # load x values, y and sigma values for all region of interests that were
+    # Load x values, y and sigma values for all region of interests that were
     # provided as masks
+    print('---Load provided parameter maps')
     lstPrmAry, objHdr, aryAff = loadNiiDataExt(cfg.lstPathNiiPrm,
                                                lstFlsMsk=cfg.lstPathNiiMask)
 
-    # deduce number of region of interest
+    # Deduce number of region of interest
     cfg.varNumRois = len(lstPrmAry)
+    print('------Number of ROIs found: ' + str(cfg.varNumRois))
 
-    # %% convert from degree to pixel
+    # Load threshold map, if desired by user
+    if cfg.strPathNiiThr:
+        # Get threshold values
+        lstThr = loadNiiDataExt([cfg.strPathNiiThr],
+                                lstFlsMsk=cfg.lstPathNiiMask)[0]
+        # Turn threshold values into boolean arrays by checking if they are
+        # above the threshold specified by the user
+        for ind, aryThr in enumerate(lstThr):
+            lstThr[ind] = np.greater_equal(aryThr, cfg.varThr)
 
-    # convert parameter maps gthat were provided in degrees of visual angle
+    # Apply threshold map, if desired by user
+    if cfg.strPathNiiThr:
+        print('---Exclude voxels based on threshold map')
+        print('------Threshold is set to: ' + str(cfg.varThr))
+        for ind, (aryPrm, aryThr) in enumerate(zip(lstPrmAry, lstThr)):
+            # Check how many voxels before selection
+            varNumVxlBfr = aryPrm.shape[0]
+            # apply threshold boolean to exclude voxels
+            lstPrmAry[ind] = aryPrm[aryThr, ...]
+            # Check how many voxels were excluded
+            varNumVxlExl = varNumVxlBfr - lstPrmAry[ind].shape[0]
+            # print number of voxels included and excluded
+            print('------Number of voxels excluded in ROI ' + str(ind+1) +
+                  ': ' + str(varNumVxlExl))
+
+    # Check how many voxels are left in ROIs and provide info to user
+    print('---Counting voxels in provided ROIs:')
+    for ind, aryPrm in enumerate(lstPrmAry):
+        # Check how many voxels before selection
+        varNumVxlIncl = aryPrm.shape[0]
+        print('------Number of voxels now included in ROI ' + str(ind+1) +
+              ': ' + str(varNumVxlIncl))
+
+    # %% Convert from degree to pixel
+
+    # Convert parameter maps that were provided in degrees of visual angle
     # to parameters in pixels, since this will be the relevant unit for the
     # visual field projection
 
     for ind, aryPrm in enumerate(lstPrmAry):
         # remap values
-        lstPrmAry[ind] = rmp_deg_pixel_x_y_s(aryPrm[:, 0], aryPrm[:, 1],
-                                             aryPrm[:, 2],
-                                             cfg.tplVslSpcPix,
-                                             int(cfg.varXminDeg),
-                                             int(cfg.varXmaxDeg),
-                                             int(cfg.varYminDeg),
-                                             int(cfg.varYmaxDeg))
+        lstPrmAry[ind] = rmp_deg_pixel_xys(aryPrm[:, 0],
+                                           aryPrm[:, 1],
+                                           aryPrm[:, 2],
+                                           cfg.tplVslSpcPix,
+                                           int(cfg.varXminDeg),
+                                           int(cfg.varXmaxDeg),
+                                           int(cfg.varYminDeg),
+                                           int(cfg.varYmaxDeg))
 
-    # %% create visual field coverage images
-    lstViFi = [None] * len(lstPrmAry)
+    # %% Create visual field coverage images
+    print('---Create visual field coverage images')
 
-    # loop over ROIs
+    # prepare list for additive and maximum Gaussian output
+    lstAddGss = [None] * len(lstPrmAry)
+    lstMaxGss = [None] * len(lstPrmAry)
+
+    # Loop over ROIs
     for indRoi, aryPrm in enumerate(lstPrmAry):
+        print('------for ROI ' + str(indRoi+1))
 
-        # prepare image
-        aryViFi = np.zeros((cfg.tplVslSpcPix))
-        # summary image
-        allImas = np.zeros((cfg.tplVslSpcPix[0], cfg.tplVslSpcPix[1],
-                            aryPrm.shape[0]))
+        # Run functyion to create visual field coverage
+        # Return both the reulst of the additive and maximum method
+        aryAddGss, aryMaxGss = crt_fov(aryPrm, cfg.tplVslSpcPix)
 
-        # loop over voxels
-        counter = 0
-        for indVxl, vecVxlPrm in enumerate(aryPrm):
-            # extract winner parameters for this voxel
-            varPosX, varPosY, varSd = vecVxlPrm[0], vecVxlPrm[1], vecVxlPrm[2]
-            if np.isclose(varSd, 0, atol=1e-04):
-                pass
-            else:
-                # recreate the winner 2D Gaussian
-                aryTmpGss = crt_2D_gauss(cfg.tplVslSpcPix[0],
-                                         cfg.tplVslSpcPix[1],
-                                         varPosX, varPosY, varSd)
-                if np.sum(np.isnan(aryTmpGss)) > 0:
-                    print('NaN NaN NaN in voxel ' + str(indVxl))
-                # add Gaussians for this region
-                aryViFi += aryTmpGss
-                # add to couner
-                counter += 1
+        # Put outputs away to list
+        lstAddGss[indRoi] = aryAddGss
+        lstMaxGss[indRoi] = aryMaxGss
 
-            # put away allImas
-            allImas[..., indVxl] = aryTmpGss
+    # %% Bootstrap the visual field coverage, if desired by user
+    
+    if cfg.varNumBts > 0:
+        print('---Create bootstrapped visual field coverage images')
 
-        # divide by total number of Gaussians that were added
-        # aryViFi /= aryPrm.shape[0]
-        aryViFi /= counter
+        # prepare list for additive and maximum Gaussian output
+        lstBtsAddGss = [None] * len(lstPrmAry)
+        lstBtsMaxGss = [None] * len(lstPrmAry)
 
-        # put way to list
-        lstViFi[indRoi] = aryViFi
+        # Loop over ROIs
+        for indRoi, aryPrm in enumerate(lstPrmAry):
+            print('------for ROI ' + str(indRoi+1))
 
-    # save visual field coverage images to disk
-    for ind, aryViFi in enumerate(lstViFi):
+            # initialize arrays that can function as accumulators of the
+            # visual field coverage map created on every bootstrap fold
+            aryBtsAddGss = np.zeros((cfg.tplVslSpcPix))
+            aryBtsMaxGss = np.zeros((cfg.tplVslSpcPix))
 
+            # get number of voxels in ROI
+            varNumVxl = aryPrm.shape[0]
+            for indFld in range(cfg.varNumBts):
+                print('---------Run bootstrap fold ' + str(indFld+1))
+                # get indices for voxels that will be sampled in this fold
+                arySmpl = bootstrap_resample(np.arange(varNumVxl))
+                # for the selected voxels, get the winner parameters
+                aryPrmRsm = aryPrm[arySmpl, :]
+                # for these winner parameters get the visual field coverage
+                aryFldAddGss, aryFldMaxGss = crt_fov(aryPrmRsm,
+                                                     cfg.tplVslSpcPix)
+                # add aryFldAddGss and aryFldMaxGss up over folds
+                aryBtsAddGss += aryFldAddGss
+                aryBtsMaxGss += aryFldMaxGss
+
+            # Put away the mean bootstrap visual field map for this particular
+            # ROI
+            lstBtsAddGss[indRoi] = np.divide(aryBtsAddGss,
+                                             float(cfg.varNumBts))
+            lstBtsMaxGss[indRoi] = np.divide(aryBtsMaxGss,
+                                             float(cfg.varNumBts))
+
+    # %% Save visual field coverage images to disk
+    print('---Save visual field coverage images to disk')
+
+    for ind in range(len(lstAddGss)):
+        
+        # get arrays
+        aryAddGss = lstAddGss[ind]
+        aryMaxGss = lstMaxGss[ind]
+        aryBtsAddGss = lstBtsAddGss[ind]
+        aryBtsMaxGss = lstBtsMaxGss[ind]
+
+        # Derive file name
         strPthFln = os.path.basename(
             os.path.splitext(cfg.lstPathNiiMask[ind])[0])
-
+        # Derive output path
         strPthImg = cfg.strPathOut + '_' + strPthFln
-        plt.imsave(strPthImg, aryViFi, cmap='viridis', format="png")
+
+        # save arrays as images
+        plt.imsave(strPthImg + '_FOV_add.png', aryAddGss, cmap='viridis',
+                   format="png", vmin=0.0, vmax = np.percentile(aryAddGss, 95))
+        plt.imsave(strPthImg + '_FOV_max.png', aryMaxGss, cmap='magma',
+                   format="png", vmin=0.0, vmax = 1.0)
+
+        plt.imsave(strPthImg + '_FOV_add_btsrp.png', aryBtsAddGss,
+                   cmap='viridis', format="png", vmin=0.0,
+                   vmax = np.percentile(aryAddGss, 95))
+        plt.imsave(strPthImg + '_FOV_max_btsrp.png', aryBtsMaxGss,
+                   cmap='magma', format="png", vmin=0.0, vmax = 1.0)
+
+    # %% Print done statement.
+    print('---Done.')

vificov/vificov_utils.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 """All utilities for vificov."""
 
@@ -22,6 +21,7 @@
 
 import numpy as np
 import scipy as sp
+import warnings
 import nibabel as nb
 
 
@@ -117,7 +117,9 @@ def loadNiiDataExt(lstFunc,
             for indAry, aryPrm in enumerate(lstPrm):
                 # get voxels specific to this mask
                 aryPrmSel[:, indAry] = aryPrm[aryMsk, ...]
-            lstPrmAry[indLst] = aryPrmSel
+            # put array away in list, if only one parameter map was provided
+            # the output will be squeezed
+            lstPrmAry[indLst] = np.squeeze(aryPrmSel)
 
     # also get header object and affine array
     # we simply take it for the first functional nii file, cause that is the
@@ -171,8 +173,8 @@ def rmp_rng(aryVls, varNewMin, varNewMax, varOldThrMin=None,
     return aryNewVls
 
 
-def rmp_deg_pixel_x_y_s(vecX, vecY, vecPrfSd, tplPngSize,
-                        varExtXmin, varExtXmax, varExtYmin, varExtYmax):
+def rmp_deg_pixel_xys(vecX, vecY, vecPrfSd, tplPngSize,
+                      varExtXmin, varExtXmax, varExtYmin, varExtYmax):
     """Remap x, y, sigma parameters from degrees to pixel.
 
     Parameters
@@ -201,6 +203,7 @@ def rmp_deg_pixel_x_y_s(vecX, vecY, vecPrfSd, tplPngSize,
         Array with possible y parametrs in pixel
     vecPrfSd : 1D numpy array
         Array with possible sd parametrs in pixel
+
     """
     # Remap modelled x-positions of the pRFs:
     vecXpxl = rmp_rng(vecX, 0.0, (tplPngSize[0] - 1), varOldThrMin=varExtXmin,
@@ -225,8 +228,10 @@ def rmp_deg_pixel_x_y_s(vecX, vecY, vecPrfSd, tplPngSize,
 
     # Convert prf sizes from degrees of visual angles to pixel
     vecPrfSdpxl = np.multiply(vecPrfSd, varDgr2PixX)
-
-    return np.column_stack((vecXpxl, vecYpxl, vecPrfSdpxl))
+    
+    # Return new values in column stack.
+    # Since values are now in pixel, they should be integer
+    return np.column_stack((vecXpxl, vecYpxl, vecPrfSdpxl)).astype(np.int32)
 
 
 def crt_2D_gauss(varSizeX, varSizeY, varPosX, varPosY, varSd):
@@ -265,8 +270,102 @@ def crt_2D_gauss(varSizeX, varSizeY, varPosX, varPosY, varSd):
         (2.0 * np.square(varSd))
         )
     aryGauss = np.exp(-aryGauss) / (2 * np.pi * np.square(varSd))
+    
+    # because we assume later (when plugging in the winner parameters) that the
+    # origin of the created 2D Gaussian was in the lower left and that the
+    # first axis of the array indexes the left-right direction of the screen
+    # and the second axis indexes the the top-down direction of the screen,
+    # we rotate by 90 degrees clockwise
+    aryGauss = np.rot90(aryGauss, k=1)
 
     return aryGauss
+
+
+def crt_fov(aryPrm, tplVslSpcPix):
+    """Create field of view for given winner x,y,sigma parameters.
+
+    Parameters
+    ----------
+    aryPrm : 2D numpy array, shape [number of voxels, 3]
+        Array with x, y, and sigma winner parameters for all voxels included in
+        a given ROI
+    tplVslSpcPix : tuple
+        Tuple with the (width, height) of the visual field in pixel.
+
+    Returns
+    -------
+    aryAddGss : 2d numpy array, shape [width, height]
+        Visual field coverage using the additive method.
+    aryMaxGss : 2d numpy array, shape [width, height]
+        Visual field coverage using maximum method.
+
+    References
+    -------
+    [1]
+
+    """
+
+    # Prepare image for additive Gaussian
+    aryAddGss = np.zeros((tplVslSpcPix))
+    # Prepare image for max Gaussian
+    aryMaxGss = np.zeros((tplVslSpcPix))
+
+    # Loop over voxels
+    varDivCnt = 0
+    for indVxl, vecVxlPrm in enumerate(aryPrm):
+        # Extract winner parameters for this voxel
+        varPosX, varPosY, varSd = vecVxlPrm[0], vecVxlPrm[1], vecVxlPrm[2]
+        # Do not continue the for-loop for voxels that have a standard
+        # deviation of 0 pixels
+        if np.isclose(varSd, 0, atol=1e-04):
+            continue
+        else:
+            # Recreate the winner 2D Gaussian
+            aryTmpGss = crt_2D_gauss(tplVslSpcPix[0], tplVslSpcPix[1],
+                                     varPosX, varPosY, varSd)
+            if np.sum(np.isnan(aryTmpGss)) > 1:
+                warnings.warn("NaN value encountered in 2D Gaussian")
+            # Add Gaussians for this region
+            aryAddGss += aryTmpGss
+            # Replace pixels for which aryTmpGss is greater than aryMaxGss
+            aryTmpGssNrm = np.divide(aryTmpGss, aryTmpGss.max())
+            lgcMaxGss = np.greater(aryTmpGssNrm, aryMaxGss)
+            aryMaxGss[lgcMaxGss] = np.copy(aryTmpGssNrm[lgcMaxGss])
+            # Add to division couner
+            varDivCnt += 1
+
+    # Divide by total number of Gaussians that were included
+    aryAddGss /= varDivCnt
+    
+    return aryAddGss, aryMaxGss
+
+
+def bootstrap_resample(aryX, varLen=None):
+    """Perform resampling via bootstrapping for an input array.
+
+    Parameters
+    ----------
+    aryX : 1D numpy array
+        Data to be resampled.
+    varLen : int, optional
+        Length of bootsrapped sample. Equal to len(aryX) if varLen==None.
+
+    Returns
+    -------
+    aryRsm : 1D numpy array
+        Bootstrapped sample of the input array.
+
+    References
+    -------
+    [1] Modified from: https://gist.github.com/aflaxman/6871948
+    
+    """
+    if varLen == None:
+        varLen = len(aryX)
+        
+    resample_i = np.floor(np.random.rand(varLen)*len(aryX)).astype(int)
+    aryRsm = aryX[resample_i]
+    return aryRsm
 
 
 class cls_set_config(object):
@@ -279,6 +378,7 @@ class cls_set_config(object):
         Dictionary containing parameter names (as keys) and parameter values
         (as values). For example, `dicCnfg['varTr']` contains a float, such as
         `2.94`.
+
     """
 
     def __init__(self, dicCnfg):