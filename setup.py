3990cd587e4d1f220f3ffb43461c0dd2a5fa53a6
MSchnei <marian.schneider@maastrichtuniversity.nl>
2018-09-04 14:27:29 +0200

    Version bump.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst') as f:
     long_description = f.read()
 
 setup(name='vificov',
-      version='1.0.5',
+      version='1.0.6',
       description=('Visual Field Coverage (ViFiCov) visualization in python.'),
       url='https://github.com/MSchnei/vificov',
       author='Marian Schneider',
